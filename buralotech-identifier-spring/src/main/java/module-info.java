a1528b7fbbccd9cb489cd61ae52de1cdecb43629
bmatthews68 <bmatthews68@gmail.com>
2024-10-14 01:28:01 +0300

    Modularise the library

--- buralotech-identifier-spring/src/main/java/module-info.java
+++ buralotech-identifier-spring/src/main/java/module-info.java
@@ -4,4 +4,5 @@ module buralotech.identifier.spring {
     requires spring.core;
     requires buralotech.identifier.core;
     exports com.buralotech.oss.identifier.spring;
+    opens com.buralotech.oss.identifier.spring to spring.core;
 }
