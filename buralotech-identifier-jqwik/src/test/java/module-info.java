34156371502de2ae787b61e2581772bfb88a0f66
bmatthews68 <bmatthews68@gmail.com>
2025-09-24 05:48:23 +0300

    Removed unnecessary require net.jqwik.engine

--- buralotech-identifier-jqwik/src/test/java/module-info.java
+++ buralotech-identifier-jqwik/src/test/java/module-info.java
@@ -2,6 +2,5 @@ open module buralotech.identifier.jqwik.test {
     requires buralotech.identifier.core;
     requires buralotech.identifier.jqwik;
     requires net.jqwik.api;
-    requires net.jqwik.engine;
     requires org.assertj.core;
 }
